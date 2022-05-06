666c57d7d9534c273be521855a08f38ea7932854
Kolynes Chinedu <kolyneschinedubinance@gmail.com>
2022-05-06 10:36:33 +0100

    update inventory view

---
+++ src/migrations/20220506093452-inventoryViewFix2.ts
@@ -0,0 +1,44 @@
+import { Db } from "mongodb";
+import { EViews, ECollections } from "../types";
+
+export const up = async (db: Db) => {
+  await db.dropCollection(EViews.inventory);
+  await db.createCollection(
+    EViews.inventory,
+    {
+      viewOn: ECollections.inventory,
+      pipeline: [
+        { $addFields: { id: "$_id" } },
+        { $project: { _id: 0 } },
+        { $match: { deleteDate: undefined } },
+      ]
+    }
+  );
+};
+
+export const down = async (db: Db) => {
+  await db.dropCollection(EViews.inventory);
+  await db.createCollection(
+    EViews.inventory,
+    {
+      viewOn: ECollections.inventory,
+      pipeline: [
+        {
+          $lookup: {
+            from: ECollections.inventory,
+            as: "items",
+            foreignField: "folder",
+            localField: "_id",
+            pipeline: [
+              { $count: "numberOfItems" },
+            ]
+          }
+        },
+        { $unwind: "$items" },
+        { $addFields: { id: "$_id" } },
+        { $project: { _id: 0 } },
+        { $match: { deleteDate: undefined } },
+      ]
+    }
+  );
+};
